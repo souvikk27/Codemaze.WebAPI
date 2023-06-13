5e7323b6e0bb9e44aa3d15a95001a9198aa733d6
Souvik Kundu <85146980+souvikk27@users.noreply.github.com>
2023-06-13 15:08:20 +0530

    Onion Architecture Implementation

--- CodeMaze.WebAPI/Extensions/ServiceExtensions.cs
+++ CodeMaze.WebAPI/Extensions/ServiceExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Contracts;
 using LoggerService;
+using Microsoft.EntityFrameworkCore;
 using Repository;
+using Service;
+using Service.Contracts;
 
 namespace CodeMaze.WebAPI.Extensions
 {
@@ -30,5 +33,12 @@ namespace CodeMaze.WebAPI.Extensions
 
         public static void ConfigureRepositoryManager(this IServiceCollection services) =>
             services.AddScoped<IRepositoryManager , RepositoryManager>();
+
+        public static void ConfigureServiceManager(this IServiceCollection services) =>
+            services.AddScoped<IServiceManager, ServiceManager>();
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(option => 
+                option.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
     }
 }
--- CodeMaze.WebAPI/Program.cs
+++ CodeMaze.WebAPI/Program.cs
@@ -14,6 +14,7 @@ builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
