d49ea73e8ac859c4fa006bcda6f3a8e14401ad73
Souvik Kundu <85146980+souvikk27@users.noreply.github.com>
2023-06-13 12:53:46 +0530

    Initial Commit

---
+++ CodeMaze.WebAPI/Program.cs
@@ -0,0 +1,40 @@
+using CodeMaze.WebAPI.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+LogManager.Setup().LoadConfigurationFromFile("nlog.config");
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseHsts(); 
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
